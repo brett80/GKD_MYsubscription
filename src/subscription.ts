4257000af2a0f529996f5a2d2ceff5f52d2b56bb
brett80 <brett80.xue@gmail.com>
2026-03-16 14:32:03 +0800

    Update subscription.ts

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,16 +4,16 @@ import categories from './categories';
 //import globalGroups, { OPEN_AD_ORDER } from './globalGroups';
 import { RawApp, RawAppGroup } from '@gkd-kit/api';
 
-const apps = await batchImportApps(`${import.meta.dirname}/apps`);
-const rawApps: RawApp[] = [];
-apps.forEach((appConfig) => {
-  appConfig.groups?.forEach((g: RawAppGroup) => {
-    if (g.name.startsWith('开屏广告')) {
-      g.order = OPEN_AD_ORDER;
-    }
-  });
-  rawApps.push(appConfig);
-});
+//const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+//const rawApps: RawApp[] = [];
+//apps.forEach((appConfig) => {
+//  appConfig.groups?.forEach((g: RawAppGroup) => {
+    //if (g.name.startsWith('开屏广告')) {
+//      g.order = OPEN_AD_ORDER;
+    //}
+//  });
+  //rawApps.push(appConfig);
+//});
 
 export default defineGkdSubscription({
   id: 16233,
