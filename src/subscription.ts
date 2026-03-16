f6a39562d95250cee5e71db4c5e260dae384743c
brett80 <brett80.xue@gmail.com>
2026-03-16 14:23:34 +0800

    初始化订阅信息

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,15 +1,27 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
 import { batchImportApps } from '@gkd-kit/tools';
 import categories from './categories';
-import globalGroups from './globalGroups';
+import globalGroups, { OPEN_AD_ORDER } from './globalGroups';
+import { RawApp, RawAppGroup } from '@gkd-kit/api';
+
+const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+const rawApps: RawApp[] = [];
+apps.forEach((appConfig) => {
+  appConfig.groups?.forEach((g: RawAppGroup) => {
+    if (g.name.startsWith('开屏广告')) {
+      g.order = OPEN_AD_ORDER;
+    }
+  });
+  rawApps.push(appConfig);
+});
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
+  id: 16233,
+  name: 'blt的GKD订阅🚀',
+  version: 1,
+  author: 'blt',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/brett80/GKD_subscription_blt/issues/new/choose',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
