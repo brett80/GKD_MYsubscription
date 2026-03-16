cc0b240f5837ce17fd9cad3b14af7c769f6f15e0
brett80 <brett80.xue@gmail.com>
2026-03-16 14:44:15 +0800

    Create com.didapinche.booking.ts

---
+++ src/apps/com.didapinche.booking.ts
@@ -0,0 +1,21 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp(
+{
+  id: 'com.didapinche.booking',
+  name: '嘀嗒出行',
+  groups: [
+    {
+      key: 1,
+      name: '弹窗广告',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'com.didapinche.booking.tab.activity.DiDaTabMainActivity',
+          matches: '@ImageView < FrameLayout <2 FrameLayout < FrameLayout < FrameLayout < FrameLayout < [id="android:id/content"]',
+        },
+      ],
+    },
+  ],
+}
+);
